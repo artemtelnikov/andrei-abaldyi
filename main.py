f4e6234e2b90895f833cd60d42a0bc1310d9c716
MIZYLINA <68012552+artemtelnikov@users.noreply.github.com>
2023-05-14 15:34:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ win_height = 500
 
 window.blit(background, (0,0))
 
-
+speed_x = 3
+speed_y = 3
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, h,w ,player_speed,):
+    def __init__(self, player_image, player_image2, player_x, player_y, h,w, player_speed,):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (h, w))
+        self.image2 = transform.scale(image.load(player_image), (h, w))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -22,14 +24,14 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
     def reset(self):
-        window.blit(self.image2, (self.rect.x2, self.rect.y2))
+        window.blit(self.image2, (self.rect.x, self.rect.y))
 
 game = True
 
 
 class Player(GameSprite):
-    def __init__(self, player_image, player_x, player_y, h,w ,player_speed):   
-        super().__init__(player_image, player_x, player_y, h,w ,player_speed)
+    def __init__(self, player_image, player_image2, player_x,  player_y, h,w, player_speed):   
+        super().__init__(player_image, player_image2, player_x, player_y, h,w, player_speed)
     def update(self):
         key_pressed = key.get_pressed()
         if key_pressed[K_DOWN] and self.rect.y > -300:
@@ -38,26 +40,44 @@ class Player(GameSprite):
         if key_pressed[K_UP] and self.rect.y < 400:
             self.rect.y -= 4
 class Player2(GameSprite):
-    def __init__(self, player_image, player_x, player_y, h,w ,player_speed):   
-        super().__init__(player_image, player_x, player_y, h,w ,player_speed)
+    def __init__(self, player_image, player_image2, player_x, player_y, h,w, player_speed):   
+        super().__init__(player_image, player_image2, player_x, player_y, h,w, player_speed)
     def update(self):
         key_pressed = key.get_pressed()
-        if key_pressed[s] and self.rect.y > -300:
+        if key_pressed[K_s] and self.rect.y > -300:
             self.rect.y += 4
             
-        if key_pressed[w] and self.rect.y < 400:
+        if key_pressed[K_w] and self.rect.y < 500:
             self.rect.y -= 4
 
-ballin = Player('vilka.png', 0, 0, 30, 100, 5)
-ballin2 = Player2('lozka.png', 0, 700, 30, 100, 5)
+
+
+ballin = Player('vilka.png', 0, 0, 30, 100, 100, 100)
+ballin2 = Player2('lozka.png', 0, 600, 30, 100, 100, 5)
+ball = GameSprite('ball.png', 100, 100, 30, 50, 50, 5)
+finish = False
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     window.blit(background, (0,0))
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y > win_height:
+        speed_y *= -1
+    if ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(ballin, ball):
+        speed_x *= -1
+    if sprite.collide_rect(ballin2, ball):
+        speed_x *= -1
 
     ballin.update()
     ballin.reset()
     ballin2.update()
     ballin2.reset()
+    ball.update()
+    ball.reset()
     display.update()
